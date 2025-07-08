2f3be62b2a1dcd054be3ffdd527f0b36dd5a03ac
Leo Borai <estebanborai@gmail.com>
2025-07-09 01:47:39 +0200

    feat: features section responsive and dividers

--- src/lib/components/landing/Features.svelte
+++ src/lib/components/landing/Features.svelte
@@ -1,7 +1,7 @@
-<section class="mb-20 space-y-16">
-	<div class="max-w-2xl">
-		<span class="labeled-text"> Communal resiliency </span>
-		<p class="article">
+<section class="mb-20 space-y-20 md:space-y-0 divide-y divide-roomy-soft-purple">
+	<div class="group flex flex-col md:flex-row justify-between items-center gap-8">
+		<span class="md:opacity-80 group-hover:opacity-100 labeled-text"> Communal resiliency </span>
+		<p class="md:opacity-80 md:group-hover:opacity-100 article">
 			Build sovereign communities, designed to persist under the harshest conditions with
 			novelties like Universally Shared Records: you will have a local copy of your
 			community's data at all times, and so will all your community members!
@@ -10,17 +10,17 @@
 			<a href="#" class="link">Appropriate Technology</a>
 		</p>
 	</div>
-	<div class="max-w-2xl">
-		<span class="labeled-text">Peer powered</span>
-		<p class="article">
+	<div class="group flex flex-col md:flex-row justify-between items-center gap-8">
+		<span class="md:opacity-80 group-hover:opacity-100 labeled-text">Peer powered</span>
+		<p class="md:opacity-80 md:group-hover:opacity-100 article">
 			Make communities that can, if necessary, endure purely on the shared resources of
 			sufficiently aligned peers in a mutual-sharing network.
 			<a href="#" class="link"> Peer-2-Peer software </a>
 		</p>
 	</div>
-	<div class="max-w-2xl">
-		<span class="labeled-text">Public-first; privacy-friendly</span>
-		<p class="article">
+	<div class="group flex flex-col md:flex-row justify-between items-center gap-8">
+		<span class="md:opacity-80 group-hover:opacity-100 labeled-text">Public-first; privacy-friendly</span>
+		<p class="md:opacity-80 md:group-hover:opacity-100 article">
 			Roomy is designed first and foremost for publicly shared, collaborative content
 			exchange. Priority conversations are carefully supported, but private spaces are not the
 			primary use case for the platform.<br /><br />
