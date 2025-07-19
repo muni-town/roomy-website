4ad80957f46e95784b199946c00b0414c6d61dee
Leo Borai <estebanborai@gmail.com>
2025-07-19 13:52:29 +0200

    feat: hero image with special text effect

--- src/lib/components/templates/landing/Hero.svelte
+++ src/lib/components/templates/landing/Hero.svelte
@@ -1,17 +1,21 @@
-<section class="section-wrapper mb-20">
-	<h1
-		class="text-roomy-light-purple mb-3 bg-clip-text text-8xl leading-none font-medium md:text-9xl"
-	>
-		MAKE ✱ SPACE
-	</h1>
-	<span class="labeled-text">Content fluidity</span>
-	<p class="article">
-		In Roomy every communication artefact can be expanded seamlessly from chat message up to a
-		wall or even stand-alone channel. Open as many new channels as you need, and let the system
-		to release when they go dormant.<br />
-		No need for discussion containers (posts, threads, channels, spaces ) to remain tied to any final
-		form.
-		<br />
-		<a href="#" class="link"> Fluid Content Containers </a>
-	</p>
+<section class="section-wrapper relative mb-10">
+	<div class="mt-40">
+		<h1
+			class="text-roomy-light-purple mb-3 flex flex-col bg-clip-text text-8xl leading-none font-medium md:flex-row md:text-9xl"
+		>
+			<span class="text-white/80">MAKE&nbsp;✱&nbsp;</span><span class=" text-white"
+				>SPACE</span
+			>
+		</h1>
+		<span class="labeled-text mt-20 md:mt-0">Content fluidity</span>
+		<p class="article">
+			In Roomy every communication artefact can be expanded seamlessly from chat message up to
+			a wall or even stand-alone channel. Open as many new channels as you need, and let the
+			system to release when they go dormant.<br />
+			No need for discussion containers (posts, threads, channels, spaces ) to remain tied to any
+			final form.
+			<br />
+			<a href="#" class="link"> Fluid Content Containers </a>
+		</p>
+	</div>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,10 @@
 	} from '$lib/components/templates/landing';
 </script>
 
-<main class="min-h-screen overflow-x-hidden text-white">
+<main class="relative min-h-screen overflow-x-hidden text-white">
+	<figure class="absolute -top-32 -right-32 -z-10 h-[840px] w-[840px]">
+		<img alt="" src="/images/e6h.png" class="opacity-60" />
+	</figure>
 	<Navigation />
 	<Hero />
 	<ContentPreview />
